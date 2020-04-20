986247f67e59cbe43c71fad47637e3a8ea39591a
Pabloin <pablo.inchausti.bf@gmail.com>
2020-04-20 17:38:01 -0300

    ID-#1 Commit Initial: PoC HealthyData for CallForCode

--- HealthyData/src/app/@theme/components/footer/footer.component.ts
+++ HealthyData/src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">With ♥ to <b><a href="https://developer.ibm.com/callforcode/getstarted/covid-19/" target="_blank">CallForCode @ 2020</a></b>Track COVID-19</span>
+    <span class="created-by">With ♥ to 
+       <b><a href="https://developer.ibm.com/callforcode/getstarted/covid-19/" target="_blank">CallForCode @ 2020</a></b>
+       &nbsp; Track COVID-19</span>
   `,
 })
 export class FooterComponent {
