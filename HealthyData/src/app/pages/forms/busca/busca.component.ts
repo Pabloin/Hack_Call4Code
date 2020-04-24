4605a4a24d3e63214c5fd8ec05dd281c85410886
Pabloin <pablo.inchausti.bf@gmail.com>
2020-04-24 14:04:10 -0300

    ID-#4 Add Interactions

--- HealthyData/src/app/pages/forms/busca/busca.component.ts
+++ HealthyData/src/app/pages/forms/busca/busca.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NbComponentShape, NbComponentSize, NbComponentStatus } from '@nebular/theme';
 
 @Component({
@@ -12,9 +14,33 @@ export class BuscaComponent implements OnInit {
   shapes: NbComponentShape[] = [ 'rectangle', 'semi-round', 'round' ];
   sizes: NbComponentSize[] = [ 'tiny', 'small', 'medium', 'large', 'giant' ];
 
+  searchingBasico: boolean;
+  searchingAvanzado: boolean;
+  textoBasico: string;
+  placeHolder: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.doClear();
   }
 
+  doSearchBasico() {
+    if (this.textoBasico === "") {
+      this.placeHolder = "ingrese un texto"
+    } else {
+      this.searchingBasico = !this.searchingBasico;
+    }
+  }
+
+  doSearchAvanzado() {
+    this.searchingAvanzado = !this.searchingAvanzado;
+  }
+
+  doClear() {
+    this.searchingBasico = false;
+    this.searchingAvanzado = false;
+    this.textoBasico = "";
+    this.placeHolder = "";
+  }
 }
